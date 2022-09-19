82e85e9b3f4ea4a9f0b2a97bd485292d04d2a618
Heath-H <heathhaupt92@gmail.com>
2022-09-19 08:19:09 -0700

    heath's week 2 assessment

--- coding-concepts.js
+++ coding-concepts.js
@@ -7,62 +7,65 @@
 
 // --------------------1) What will this log?
 
-const cohort = "Foxtrot 2022"
+// const cohort = "Foxtrot 2022"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will split the string of "Foxtrot 2022" into individual letters as strings "F" "o" "x" "t" so on and so forth 
+// b) Verify and explain: I was correct
+// the output was: ['F', 'o', 'x', 't','r', 'o', 't', ' ', '2', '0', '2', '2'] because .split creates an array of strings 
 
 // --------------------2) What will this log?
 
-const greeter = (name) => {
-  `Hello, ${name}!`
-}
-// console.log(greeter("LEARN Student"))
+// const greeter = (name) => {
+//   `Hello, ${name}!`
+// }
+ 
+//console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: 'Hello, LEARN Student' because LEARN Student 
+// // b) Verify and explain: undefined.. this is because name is not defined and the function doesnt have a return line, but this isn't very clear to me
 
-// --------------------3) What will this log?
+// // --------------------3) What will this log?
 
-const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
+// const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
 // console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: this will return an array with each element in the array multiplied by two [8, 10, 12, 14, 16] 
+// // b) Verify and explain: my answer was correct, this is because we used the .map to iterate over each element. The word <number> is the type of value that is in the array. <number *2> is saying that we are multiplying each value by 2.
 
-// --------------------4) What will this log?
+// // --------------------4) What will this log?
 
-const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
+// const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: [11, 13, 15]
+// // b) Verify and explain: I was correct. similarly to question 3, we are using .filter, another method, that will filter through the array of onlyOdds. Then, only the number in the array that DO have a remainder when divided by 2 are displayed. ( if you wanted the even numbers you would use % 2 === 0)
 
-// --------------------5) What will this log?
+// // --------------------5) What will this log?
 
-const myCodingSkills = {
-  languages: ["JavaScript", "Ruby"],
-  frameworks: ["React", "Ruby on Rails"],
-  databases: "PostgreSQL",
-  versionControl: "GitHub"
-}
+// const myCodingSkills = {
+//   languages: ["JavaScript", "Ruby"],
+//   frameworks: ["React", "Ruby on Rails"],
+//   databases: "PostgreSQL",
+//   versionControl: "GitHub"
+//  }
 // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: "Javascript"
+// // b) Verify and explain: I was correct. the above console.log is calling for the first index in the array given for the key <languages>
 
-// --------------------STRETCH: What will this log?
+// // --------------------STRETCH: What will this log?
 
-class Learn {
-  constructor(name) {
-    this.student = name,
-    this.cohort = "Foxtrot",
-    this.year = 2022
-  }
-}
-const learnStudent = new Learn("George")
+// class Learn {
+//   constructor(name) {
+//     this.student = name,
+//     this.cohort = "Foxtrot",
+//     this.year = 2022
+//   }
+// }
+// const learnStudent = new Learn("George")
 // console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: student: 'George', cohort: 'Foxtrot', year: 2022
+
+// // b) Verify and explain: I was correct! const learnStudent = new Learn("George") was creating a new constructor, 
--- interview-questions.md
+++ interview-questions.md
@@ -8,33 +8,33 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: A parameter is kind of like a place holder inside a function, and once you console.log call on the function, you will use an arguement to put into the spot of the parameter
 
-Researched answer:
+Researched answer: A parameter is a place holder variable that is is in the parenthesis of a function. it is later assigned a value, which is an arguement. it is passed to the parameter that was inside the parenthesis of the function, and the output will change based on the input.
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: The predefined parameters are built in methods in higher order functions. The higher order functions can take more than one arguement.
 
-Researched answer:
+Researched answer: THe predefined parameters are value, index, and array. The value of the array is required; while index and array are optional.
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: Map is a method inside the function that will bring back the same length of the original array (for example, great for modifying an array with multiplcation). filter will filter through the array and bring back what its supposed to bring back (for example, you can filter through an array of numbers and bring back odd numbers)
 
 Researched answer:
 
 4. What is iteration?
 
-Your answer:
+Your answer: the .map() method iterates through an array, and will return a new array of the same length. .filter() method iterates through an array and returns the values that are stated in the condition, filtering through the original array.
 
 Researched answer:
 
 5. What is the difference between a class and an object?
 
-Your answer:
+Your answer: An object is a collection of data that stores keys, while a class acts like a blueprint for an object. 
 
-Researched answer:
+Researched answer: Objects are a data type that has collections of key value pairs. Objects are like an intersection of data and behavior. Classes are a type of function that contain data and behavior. 
 
 6. STRETCH: What is hoisting in JavaScript?
 
@@ -46,12 +46,12 @@ Researched answer:
 
 Research and define the following terms to the best of your ability.
 
-1. User stories:
+1. User stories: what a user can see on a website (header, footer etc)
 
 2. Spread operator:
 
-3. React:
+3. React: a library used to build webpages, mobile pages, and native mobile apps. it is used to build small parts, and then join them together to form a bigger thing (webpage, app)
 
-4. React props:
+4. React props:ngive us ability to communicate between components by passing data and behavior from one component to a nested component
 
-5. DOM events:
+5. DOM events: something that allows you yo register event handlers on elements in functions. the function wont be executed before the event occurs (clicking a button, maybe hovering over something on a website can be a dom event that causes something to display on the screen etc)
